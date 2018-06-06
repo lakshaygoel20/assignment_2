ede0077c6cea101e4a6b7b28fae07e6e7373862e
lakshaygoel20 <39944568+lakshaygoel20@users.noreply.github.com>
2018-06-07 00:19:04 +0530

    Add files via upload

---
+++ 2assignment.py
@@ -0,0 +1,28 @@
+#gquestion 1
+print("hello world")
+print(12*"#")
+#question 2
+x="hello"
+y="world"
+print(x+y)
+print(12*"#")
+#question 3
+a=input("enter your name:")
+b=input("enter your age:")
+c=input("enter your city:")
+print(a,b,c,sep=':')
+print(12*"#")
+#question 4
+print('''"let's get started"''')
+print(12*"#")
+#question 5
+s="acadview"
+course="python"
+fee=5000
+print('''s="%s" \ncourse="%s" \nfees=%d'''%(s,course,fee))
+print(12*"#")
+#question 6
+name="tony stark"
+salary=1000000
+print('%s''%d'%(name,salary))
+print(12*"#")
