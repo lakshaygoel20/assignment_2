06980942906bd65a8dad6c12756fdaab57548045
lakshaygoel20 <lakshaygoel20@gmail.com>
2018-06-13 00:00:39 +0530

    commit is done

---
+++ datatype_1.py
@@ -0,0 +1,43 @@
+#question 1
+list_1=["apple","banana","peach","apple"]
+print("the list is :",list_1)
+print("#"*15)
+#question  2
+list_2=["google","apple","facebook","microsoft","tesla"]
+list_3=list_1+list_2
+print("the new list is:",list_3)
+print("#"*15)
+#question  3
+print('''no. of times "apple" appear on list is:''',list_3.count("apple"))
+print("#"*15)
+#question  4
+list_4=['2','3','1','8','4','0','9','5']
+list_4.sort()
+print("list sorted in ascending order is: ",list_4)
+print("#"*15)
+#question 5
+print("first sorted array is :",list_4)
+list_1=['2','4','1','9','6','8','7']
+list_1.sort()
+print("2nd sorted list is:",list_1)
+list_2=list_4+list_1
+list_2.sort()
+print("the third sorted list is:",list_2)
+print("#"*15)
+#question 6
+list_3=['c','c++','java','python']
+print("the stack is :",list_3)
+list_3.append('.net')
+print("the new(pushed) stack is :",list_3)
+list_3.pop()
+print("the new(poped) stack is :",list_3)
+#queue
+list_1=["c","c++","java","python"]
+print("the queue is :",list_1)
+list_1.append('machine learning')
+print("the new(pushed) queue is :",list_1)
+list_1.pop(0)
+print("the new(poped) queue is :",list_1)
+print("#"*15)
+
+#question
---
+++ datatype_2.py
@@ -0,0 +1,20 @@
+#question 1
+tup_1=('apple','banana','orange','peach')
+print("the length of string is : ",len(tup_1))
+print("#"*15)
+#question 2
+print("the maximum elements is:",max(tup_1))
+print("the minimum elements is:",min(tup_1))
+print("#"*15)
+#question 3
+print("#"*15)
+#question 4
+set_1={'2','3','7','4'}
+set_2={'1','3','5','2','6','8'}
+print('the set 1 is :',set_1)
+print('the set 2 is :',set_2)
+print("the diff. of two set is :",set_1-set_2)
+print("the intersection  of two set is :",set_1 & set_2)
+print("the comparison of two set is :",set_1==set_2)
+print("#"*15)
+
